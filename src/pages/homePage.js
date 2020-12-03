34330e0c3568a16fb449361c9b355e2a81f8c21d
WaiHin0 <skylamin95@gmail.com>
2020-12-03 11:20:51 +0630

    #Fixed Error

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     return (
-        <div>
+        <div className="text-center">
             <h2>Home Page</h2>
+            <Link to='/login'>Login</Link>
+            <Link to='/register' className="ml-3">Register</Link>
         </div>
     )
 }
